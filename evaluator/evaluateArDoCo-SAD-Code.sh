137fced4cac3e218105d9140a604639ca67962db
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-08-01 10:37:56 +0200

    Add raw results for direct execution of ArDoCo

--- evaluator/evaluateArDoCo-SAD-Code.sh
+++ evaluator/evaluateArDoCo-SAD-Code.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
+echo "#### TRANSITIVE ####"
+
 echo "BigBlueButton"
-java -jar evaluator.jar -t ../results/raw/ArDoCo/sadCodeTlr_bigbluebutton.csv -g ../data/SAD-Code-goldstandards/goldstandard-bigbluebutton.csv -c 47600
+java -jar evaluator.jar -t ../results/raw/ArDoCo/transitive/sadCodeTlr_bigbluebutton.csv -g ../data/SAD-Code-goldstandards/goldstandard-bigbluebutton.csv -c 47600
 
 echo "JabRef"
-java -jar evaluator.jar -t ../results/raw/ArDoCo/sadCodeTlr_jabref.csv -g ../data/SAD-Code-goldstandards/goldstandard-jabref.csv -c 26000
+java -jar evaluator.jar -t ../results/raw/ArDoCo/transitive/sadCodeTlr_jabref.csv -g ../data/SAD-Code-goldstandards/goldstandard-jabref.csv -c 26000
 
 echo "MediaStore"
-java -jar evaluator.jar -t ../results/raw/ArDoCo/sadCodeTlr_mediastore.csv -g ../data/SAD-Code-goldstandards/goldstandard-mediastore.csv -c 3589
+java -jar evaluator.jar -t ../results/raw/ArDoCo/transitive/sadCodeTlr_mediastore.csv -g ../data/SAD-Code-goldstandards/goldstandard-mediastore.csv -c 3589
 
 echo "TEAMMATES"
-java -jar evaluator.jar -t ../results/raw/ArDoCo/sadCodeTlr_teammates.csv -g ../data/SAD-Code-goldstandards/goldstandard-teammates.csv -c 165330
+java -jar evaluator.jar -t ../results/raw/ArDoCo/transitive/sadCodeTlr_teammates.csv -g ../data/SAD-Code-goldstandards/goldstandard-teammates.csv -c 165330
 
 echo "TeaStore"
-java -jar evaluator.jar -t ../results/raw/ArDoCo/sadCodeTlr_teastore.csv -g ../data/SAD-Code-goldstandards/goldstandard-teastore.csv -c 8815
+java -jar evaluator.jar -t ../results/raw/ArDoCo/transitive/sadCodeTlr_teastore.csv -g ../data/SAD-Code-goldstandards/goldstandard-teastore.csv -c 8815
+
+echo "#### DIRECT ####"
+
+echo "BigBlueButton"
+java -jar evaluator.jar -t ../results/raw/ArDoCo/direct/sadCodeTlr_bigbluebutton.csv -g ../data/SAD-Code-goldstandards/goldstandard-bigbluebutton.csv -c 47600
+
+echo "JabRef"
+java -jar evaluator.jar -t ../results/raw/ArDoCo/direct/sadCodeTlr_jabref.csv -g ../data/SAD-Code-goldstandards/goldstandard-jabref.csv -c 26000
+
+echo "MediaStore"
+java -jar evaluator.jar -t ../results/raw/ArDoCo/direct/sadCodeTlr_mediastore.csv -g ../data/SAD-Code-goldstandards/goldstandard-mediastore.csv -c 3589
+
+echo "TEAMMATES"
+java -jar evaluator.jar -t ../results/raw/ArDoCo/direct/sadCodeTlr_teammates.csv -g ../data/SAD-Code-goldstandards/goldstandard-teammates.csv -c 165330
+
+echo "TeaStore"
+java -jar evaluator.jar -t ../results/raw/ArDoCo/direct/sadCodeTlr_teastore.csv -g ../data/SAD-Code-goldstandards/goldstandard-teastore.csv -c 8815
 
 read -p "Press Enter To Close..."
